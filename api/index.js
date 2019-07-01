98c3799b2fbee3d57d8cbef2a83337a4aa91fc5c
hxdegawa <hxdegawa@gmail.com>
2019-07-01 22:20:01 +0900

    WIP Implemented discord auth

--- api/index.js
+++ api/index.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const request = require("request-promise");
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", function(req, res) {
-res.send("HelloWorld");
-});
+  if (!req.query) res.send("Authing");
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.status(200).json({ 'message': req.body })
-})
+  const discordQuery = {
+    uri: 'https://radiko.jp/v2/api/auth1',
+    transform: (body, response, resolveWithFullResponse) => {
+      return response
+    },
+
+    headers: {
+      'client_id': process.env.WHIPPLE_CLIENT_ID,
+      'client_secret': process.env.WHIPPLE_CLIENT_SECRET,
+      'grant_type': 'authorization_code',
+      'code': req.query.code,
+      'redirect_uri': process.env.REDIRECT_URI,
+      'scope': 'identify email connections'
+    },
+    json: true
+  };
+
+  request(discordQuery)
+  .then(data => res.send(data))
+  .catch(data => res.send(data));
+});
 
 module.exports = {
 path: "/api/login",
