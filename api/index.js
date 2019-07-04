d527e2aca104524ad1f4377340976bb15764994a
hxdegawa <hxdegawa@gmail.com>
2019-07-04 15:30:20 +0900

    fix

--- api/index.js
+++ api/index.js
@@ -23,13 +23,12 @@ app.get("/", async function(req, res) {
     body: {
       'client_id': process.env.WHIPPLE_CLIENT_ID,
       'client_secret': process.env.WHIPPLE_CLIENT_SECRET,
-      'grant_type': 'client_credentials',
+      'grant_type': 'authorization_code',
       'code': req.query.code,
       'redirect_uri': process.env.REDIRECT_URI,
       'scope': 'identify email',
     },
     header: {
-      'Content-Type': 'application/x-www-form-urlencoded'
     },
     json: true
   };
